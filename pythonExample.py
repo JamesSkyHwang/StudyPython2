830a0675b7f5a62542a52792e0c4e02b81ec2d6a
JamesSkyHwang <hjw040@gmail.com>
2019-10-22 16:45:30 +0900

    완료

---
+++ pythonExample.py
@@ -0,0 +1,16 @@
+#C:\doit>python tabto4.py a.txt b.txt
+
+import sys
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+f = open(src)
+tab_content = f.read()
+f.close()
+
+space_content = tab_content.replace("\t", " "*4)
+
+f = open(dst, 'w')
+f.write(space_content)
+f.close()
